908b066c2adad87bb805b6ab8b938bfac1d74445
Vivek Vilas Patil <vivekpatil@INVivekpat.local>
2014-08-19 19:44:00 +0530

    Modified Source And Tests Minor Refactoring

--- src/src/Unit.java
+++ src/src/Unit.java
@@ -1,34 +1,30 @@
-/**
- * Created by dianadevasia on 19/08/14.
- */
 public class Unit {
 
-    private final String name;
-    private final double conversionFactor;
-    private final String type;
+	private final String name;
+	private final double conversionFactor;
+	private final String type;
 
-    static final Unit FEET = new Unit("ft", 12,"Length");
-    static final Unit YARD = new Unit("yd", 36,"Length");
-    static final Unit INCH = new Unit("in", 1,"Length");
-    static final Unit MILE = new Unit("ml", 2,"Length");
-    static final Unit TSP = new Unit("tsp", 1,"Weight");
-    static final Unit TBSP = new Unit("tbsp", 3,"Weight");
-    static final Unit OZ = new Unit("oz", 6,"Weight");
-    static final Unit CUP = new Unit("cup", 48,"Weight");
+	static final Unit FEET = new Unit("ft", 12, "Length");
+	static final Unit YARD = new Unit("yd", 36, "Length");
+	static final Unit INCH = new Unit("in", 1, "Length");
+	static final Unit MILE = new Unit("ml", 2, "Length");
+	static final Unit TSP = new Unit("tsp", 1, "Weight");
+	static final Unit TBSP = new Unit("tbsp", 3, "Weight");
+	static final Unit OZ = new Unit("oz", 6, "Weight");
+	static final Unit CUP = new Unit("cup", 48, "Weight");
 
 
-    private Unit(String name, double conversionFactor, String type) {
-        this.name = name;
-        this.conversionFactor = conversionFactor;
-        this.type = type;
-    }
+	private Unit(String name, double conversionFactor, String type) {
+		this.name = name;
+		this.conversionFactor = conversionFactor;
+		this.type = type;
+	}
 
-    public double convertTo(double amount) {
-        return this.conversionFactor * amount;
-    }
+	public double convertTo(double amount) {
+		return this.conversionFactor * amount;
+	}
 
-    public boolean compareTypes(Unit other)
-    {
-        return this.type.equals(other.type);
-    }
+	public boolean compareTypes(Unit other) {
+		return this.type.equals(other.type);
+	}
 }
--- src/tests/QuantityTest.java
+++ src/tests/QuantityTest.java
@@ -1,59 +1,55 @@
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
-// job: understands comparision of units
+// job: understands comparison of units
 public class QuantityTest {
 
-    Quantity Four_ft;
-    Quantity Four_ft_Other;
-    Quantity FourtyEight_Yards;
-    Quantity Four_tbsp;
-    Quantity Eight_oz;
-    Quantity One_cup;
-    Quantity Four_Eight_Inches;
-    Quantity Twelve_Tsp;
-    Quantity OneFortyFour_Feet;
+	Quantity Four_ft;
+	Quantity Four_ft_Other;
+	Quantity FourtyEight_yards;
+	Quantity Four_tbsp;
+	Quantity Eight_oz;
+	Quantity One_cup;
+	Quantity Four_Eight_Inches;
+	Quantity Twelve_Tsp;
+	Quantity OneFortyFour_Feet;
 
 
-    @Before
-    public void setUp() {
-        Four_ft = new Quantity(4, Unit.FEET);
-        Four_ft_Other = new Quantity(4, Unit.FEET);
-        FourtyEight_Yards = new Quantity(48, Unit.YARD);
-        Four_tbsp = new Quantity(4, Unit.TBSP);
-        Eight_oz = new Quantity(8, Unit.OZ);
-        One_cup = new Quantity(1, Unit.CUP);
-        Four_Eight_Inches = new Quantity(48, Unit.INCH);
-        Twelve_Tsp = new Quantity(12, Unit.TSP);
-        OneFortyFour_Feet = new Quantity(144, Unit.FEET);
-    }
+	@Before
+	public void setUp() {
+		Four_ft = new Quantity(4, Unit.FEET);
+		Four_ft_Other = new Quantity(4, Unit.FEET);
+		FourtyEight_yards = new Quantity(48, Unit.YARD);
+		Four_tbsp = new Quantity(4, Unit.TBSP);
+		Eight_oz = new Quantity(8, Unit.OZ);
+		One_cup = new Quantity(1, Unit.CUP);
+		Four_Eight_Inches = new Quantity(48, Unit.INCH);
+		Twelve_Tsp = new Quantity(12, Unit.TSP);
+		OneFortyFour_Feet = new Quantity(144, Unit.FEET);
+	}
 
-    @Test
-    public void checkForSameUnit() {
-        assertTrue(Four_ft.equals(Four_ft_Other));
-        assertTrue(Four_tbsp.equals(Four_tbsp));
+	@Test
+	public void checkForSameUnit() {
+		assertTrue(Four_ft.equals(Four_ft_Other));
+		assertTrue(Four_tbsp.equals(Four_tbsp));
 
-    }
+	}
 
-    @Test
-    public void checkForDifferentUnit() {
-        assertTrue(FourtyEight_Yards.equals(OneFortyFour_Feet));
-        assertTrue(Four_ft.equals(Four_Eight_Inches));
-        assertTrue(Four_tbsp.equals(Twelve_Tsp));
-        assertTrue(Eight_oz.equals(One_cup));
-        assertFalse(Eight_oz.equals(FourtyEight_Yards));
-    }
+	@Test
+	public void checkForDifferentUnit() {
+		assertTrue(FourtyEight_yards.equals(OneFortyFour_Feet));
+		assertTrue(Four_ft.equals(Four_Eight_Inches));
+		assertTrue(Four_tbsp.equals(Twelve_Tsp));
+		assertTrue(Eight_oz.equals(One_cup));
+		assertFalse(Eight_oz.equals(FourtyEight_yards));
+	}
 
-    @Test
-    public void checkForAdditionOfUnits() {
-        assertEquals(true, Four_ft.add(Four_ft) == 8.0);
-        assertEquals(true, Eight_oz.add(Four_tbsp) == 60.0);
-        assertEquals(true, Eight_oz.add(Four_ft) == -1.0);
-    }
+	@Test
+	public void checkForAdditionOfUnits() {
+		assertEquals(true, Four_ft.add(Four_ft) == 8.0);
+		assertEquals(true, Eight_oz.add(Four_tbsp) == 60.0);
+		assertEquals(true, Eight_oz.add(Four_ft) == -1.0);
+	}
 }
